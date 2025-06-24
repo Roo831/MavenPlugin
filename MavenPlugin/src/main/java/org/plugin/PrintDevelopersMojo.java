4a328eaff871db8a46b6b7a348f354a735acb778
Poptsov Roman <poptsov.roman@mail.ru>
2025-06-24 10:10:32 +0500

    eliminate potentials NullPointerException, add lifecyclePhase, change log to warn

--- MavenPlugin/src/main/java/org/plugin/PrintDevelopersMojo.java
+++ MavenPlugin/src/main/java/org/plugin/PrintDevelopersMojo.java
@@ -1,15 +1,19 @@
 package org.plugin;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.model.Developer;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-@Mojo(name = "print-developers", requiresProject = true)
+@Mojo(name = "print-developers", requiresProject = true, defaultPhase = LifecyclePhase.VALIDATE)
 public class PrintDevelopersMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
@@ -18,20 +22,38 @@ public class PrintDevelopersMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException {
         List<Developer> developers = project.getOriginalModel().getDevelopers();
 
-        if (developers == null || developers.isEmpty()) {
-            getLog().info("В проекте нет информации о разработчиках.");
+        if (CollectionUtils.isEmpty(developers)) {
+            getLog().warn("В проекте нет информации о разработчиках.");
             return;
         }
 
         getLog().info("Информация о разработчиках:");
-        for (Developer dev : developers) {
-            getLog().info("------------------------------");
-            getLog().info("ID: " + dev.getId());
-            getLog().info("Имя: " + dev.getName());
-            getLog().info("Email: " + dev.getEmail());
-            getLog().info("Организация: " + dev.getOrganization());
-            getLog().info("Роли: " + String.join(", ", dev.getRoles()));
-            getLog().info("Часовой пояс: " + dev.getTimezone());
-        }
+        developers.forEach(this::printDeveloperInfo);
+    }
+
+    private void printDeveloperInfo(Developer dev) {
+        getLog().info("------------------------------");
+        logField("ID", dev.getId());
+        logField("Имя", dev.getName());
+        logField("Email", dev.getEmail());
+        logField("Организация", dev.getOrganization());
+        logRoles(dev.getRoles());
+        logField("Часовой пояс", dev.getTimezone());
+        logField("URL", dev.getUrl());
+    }
+
+    private void logField(String fieldName, String value) {
+        String output = StringUtils.defaultIfBlank(value, "не указано");
+        getLog().info(fieldName + ": " + output);
+    }
+
+    private void logRoles(List<String> roles) {
+        String rolesOutput = CollectionUtils.isEmpty(roles)
+                ? "не указаны"
+                : roles.stream()
+                .filter(StringUtils::isNotBlank)
+                .collect(Collectors.joining(", "));
+
+        getLog().info("Роли: " + rolesOutput);
     }
 }
